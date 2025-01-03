8349e779e97c320307a919a3abf6727827717398
Sobiya Sivakumar <132220177+sobiyasiva@users.noreply.github.com>
2025-01-03 10:48:48 +0530

    Create index.js

---
+++ Todo-react/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Main from './components/Main';
+// import MainApp from './components/MainApp';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+<Main/>
+);
+
+
