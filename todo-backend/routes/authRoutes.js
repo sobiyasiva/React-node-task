d9244799fa141de99dcdb73bb37d07f5d80e8252
Sobiya Sivakumar <132220177+sobiyasiva@users.noreply.github.com>
2025-01-06 11:16:07 +0530

    Update authRoutes.js

--- todo-backend/routes/authRoutes.js
+++ todo-backend/routes/authRoutes.js
@@ -1,10 +1,8 @@
 const express = require('express');
-const AuthController = require('../controllers/authController'); // Correct import for the AuthController
+const AuthController = require('../controllers/authController'); 
 const router = express.Router();
 
 router.post('/signup', AuthController.signup);
 router.post('/login', AuthController.login);
-// router.post('/todos',AuthController.todo);
-// New refresh token route
 router.post('/refresh-token', AuthController.refreshToken);
 module.exports = router;
