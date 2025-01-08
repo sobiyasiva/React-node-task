348c92b4a1bf6e69e3dc5c21267a55b14e451157
Sobiya Sivakumar <132220177+sobiyasiva@users.noreply.github.com>
2025-01-08 17:36:40 +0530

    Update taskRoutes.js

--- todo-backend/routes/taskRoutes.js
+++ todo-backend/routes/taskRoutes.js
@@ -2,8 +2,8 @@ const MainController = require('../controllers/MainController');
 const express = require('express');
 const router = express.Router();
 
-router.post('/tasks', MainController.authenticateUser, MainController.createTask);
-router.get('/tasks', MainController.authenticateUser, MainController.getTasks);
-router.put('/tasks/:taskId', MainController.authenticateUser, MainController.updateTask);
-router.delete('/tasks/:taskId', MainController.authenticateUser, MainController.deleteTask);
+router.post('/addtasks', MainController.authenticateUser, MainController.createTask);
+router.get('/gettasks', MainController.authenticateUser, MainController.getTasks);
+router.put('/updatetasks/:taskId', MainController.authenticateUser, MainController.updateTask);
+router.delete('/deletetasks/:taskId', MainController.authenticateUser, MainController.deleteTask);
 module.exports = router;
