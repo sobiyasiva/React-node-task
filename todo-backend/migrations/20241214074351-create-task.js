c8caa8ca9698cab7b121372a0e2ca116997dac0d
Sobiya Sivakumar <132220177+sobiyasiva@users.noreply.github.com>
2025-01-30 10:23:28 +0530

    Update 20241214074351-create-task.js

--- todo-backend/migrations/20241214074351-create-task.js
+++ todo-backend/migrations/20241214074351-create-task.js
@@ -35,7 +35,7 @@ module.exports = {
           type: Sequelize.INTEGER,
           allowNull: false,  
           references: {
-            model: 'users', 
+            model: 'Users', 
             key: 'id',      
           },
           onUpdate: 'CASCADE', 
