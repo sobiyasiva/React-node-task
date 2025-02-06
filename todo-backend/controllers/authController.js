e71011b046b0a8531e6c932c61aba19da580b598
Sobiya Sivakumar <132220177+sobiyasiva@users.noreply.github.com>
2025-02-06 15:20:53 +0530

    Update authController.js

--- todo-backend/controllers/authController.js
+++ todo-backend/controllers/authController.js
@@ -5,17 +5,17 @@ class AuthController {
     try {
       const { email, password } = req.body;
       const newUserResponse = await AuthService.signup(email, password);
-      res.status(201).json(newUserResponse); 
+      res.status(201).json(newUserResponse);
     } catch (error) {
       console.error('Signup Error:', error);
-      res.status(400).json({ status: 'fail', message: error.message });
+      res.status(500).json({ status: 'fail', message: error.message });
     }
   }
   static async login(req, res) {
     try {
       const { email, password } = req.body;
       const loginResponse = await AuthService.login(email, password);
-      res.json(loginResponse); 
+      res.status(200).json(loginResponse);
     } catch (error) {
       res.status(500).json({ status: 'fail', message: error.message });
     }
@@ -24,14 +24,11 @@ class AuthController {
   static async refreshToken(req, res) {
     try {
       const { refreshToken } = req.body;
-
       if (!refreshToken) {
         return res.status(400).json({ status: 'fail', message: 'Refresh token is required' });
       }
-
       const { userId } = AuthService.verifyRefreshToken(refreshToken);
       const newToken = AuthService.generateAccessToken(userId);
-
       res.json({
         status: 'success',
         message: 'New access token generated',
