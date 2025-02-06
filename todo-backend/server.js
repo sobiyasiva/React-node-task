1313755056bae9d1a5779988d7c1dca3a6ad6154
Sobiya Sivakumar <132220177+sobiyasiva@users.noreply.github.com>
2025-02-06 15:26:08 +0530

    Update server.js

--- todo-backend/server.js
+++ todo-backend/server.js
@@ -1,24 +1,29 @@
 const express = require('express');
 const cors = require('cors');
-// const sequelize = require('./config/database');
-const authRoutes = require('./routes/authRoutes');
-const taskRoutes = require('./routes/taskRoutes');
+
 const app = express();
+const router = express.Router(); 
 
 console.log('Initializing CORS middleware...');
 app.use(
   cors({
-    origin: 'http://localhost:3000', 
+    origin: 'http://localhost:3000',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization', 'userid'],
   })
 );
+
 console.log('Initializing JSON parser middleware...');
-app.use(express.json()); 
+app.use(express.json());
+
 console.log('Registering routes...');
-app.use('/api/user', authRoutes);
-app.use('/api/task', taskRoutes);
-const PORT = process.env.PORT;
+require('./routes/authRoutes')(router); 
+require('./routes/taskRoutes')(router); 
+
+app.use('/api/user', router); 
+app.use('/api/task', router); 
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
